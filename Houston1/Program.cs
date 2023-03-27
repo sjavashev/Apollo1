754373e957c91e308e7df4c77314899103e5b04e
Stanimir Javashev <sjavashev@gmail.com>
2023-03-27 12:25:20 +0300

    Final changes

--- Houston1/Program.cs
+++ Houston1/Program.cs
@@ -2,7 +2,11 @@
 var app = builder.Build();
 
 app.MapGet("/", () => new HoustonAnswer(StatusCode: 1001, Text: "This is Houston!"));
-app.Urls.Add("https://192.168.1.64:16000/");
+
+#if DEBUG
+app.Urls.Add(MissionShared1.Tools.ApiClient.BASE_URL + "/");
+#endif
+
 app.Run();
 
 public record HoustonAnswer(int StatusCode, string Text);
---
+++ MissionShared1/Tools/ApiClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+
+namespace MissionShared1.Tools {
+	public partial class ApiClient {
+		public static async Task<HttpResponseMessage> GetAsync(string sub_url) {
+			var url = String.Join("/", new[] { BASE_URL, sub_url?.Trim('/') });
+			var res = await _http_client.GetAsync(url);
+			return res;
+		}
+
+		private ApiClient() { }
+
+		static ApiClient() {
+#pragma warning disable CS0162
+			if (IS_MOCK_SSL) {
+				ServicePointManager.ServerCertificateValidationCallback += delegate { return true; };
+				_http_client = new HttpClient(new HttpClientHandler() {
+					ServerCertificateCustomValidationCallback = delegate { return true; },
+				});
+			}
+			else {
+				_http_client = new HttpClient();
+			}
+#pragma warning restore CS0162
+		}
+
+		static HttpClient _http_client;
+
+#if !DEBUG
+		public const string BASE_URL = "https://my.real.backendurl.com:16384";
+		const bool IS_MOCK_SSL = false;
+#endif
+	}
+}
+
